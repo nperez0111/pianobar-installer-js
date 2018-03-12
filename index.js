b91330e0283c8018508d46325633c6aa98caa409
Nick Perez <nperez0111@gmail.com>
2018-03-12 14:34:38 -0700

    Better messages and more clear code

--- index.js
+++ index.js
@@ -6,54 +6,65 @@ const commandExists = require( 'command-exists' ),
     logToFile = shared.logToAFile,
     readFile = shared.readFile,
     brew = execa.bind( execa, 'brew' ),
-    homedir = require( 'homedir' ),
-    path = homedir() + '/.config/pianobar',
+    mkfifo = execa.bind(execa, 'mkfifo'),
+    installBrew = () => execa('/usr/bin/ruby', ['-e', '"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"']),
+    homedir = require('homedir')(),
+    pianobarConfigPath = path.resolve(homedir, '.config/pianobar'),
+    configPath = path.resolve(pianobarConfigPath, 'config'),
+    ctlPath = path.resolve(pianobarConfigPath, 'ctl'),
     config = require( './config' ),
     err = shared.err,
     makedirIfNotExists = shared.makedirIfNotExists,
     youSure = require( './youSure' ),
     advancedInstall = require( './advancedInstall' ),
-    run = () => {
-        commandExists( 'pianobar' ).catch( () => {
+  run = () => {
+    log("Checking to see if Pianobar is installed...")
+    commandExists('pianobar').catch(() => {
 
-            log( `Pianobar doesn't exist so let's install it...` )
+        log(`Pianobar is not installed, So let's install it...`)
+        log("Checking to see if Brew is installed...")
+        commandExists('brew').catch(() => {
 
-            commandExists( 'brew' ).catch( () => {
+            log(`Brew is not installed, So lets install it...`)
 
-                log( `Brew doesn't exists so lets install it...` )
+            return installBrew().catch(err('Error installing brew...'))
+        }).then(() => {
 
-                return execa( '/usr/bin/ruby', [ '-e', '"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"' ] ).catch( err( 'Brew' ) )
-            } ).then( () => {
+            log(`Success!\n So, let's update Brew it and install Pianobar...`)
 
-                log( `Brew is installed, So let's update it and install pianobar...` )
+            return brew(['update'])
+                .catch(err('brew update failed'))
+                .then(() => log("Attempting to install Pianobar"))
+                .then(brew(['install', 'pianobar']))
+                .then(() => { log("Success!") })
+                .catch(err('Pianobar failed to install'))
+        })
+    }).then(() => {
+        //pianobar exists
+        log(`Ensuring directory structure...`)
 
-                return brew( [ 'update' ] )
-                    .catch( err( 'brew update' ) )
-                    .then( brew( [ 'install', 'pianobar' ] ) )
-                    .catch( err( 'Pianobar' ) )
-            } )
-        } ).then( () => {
+        return makedirIfNotExists(path).then(() => log("Success!")).catch(err("Failed..."))
+    }).then(() => {
+        log(`Attempting to create FIFO...`)
+        return mkfifo(ctlPath)
+            .catch(a => {
+                log("FIFO is probably already there...")
+            }).then(() => {
+                log('Success!')
+            })
+    }).then(() => {
+        log("Generating Pianobar config file based off of the following questions...")
 
-            log( `Pianobar exists, let's keep going...` )
-
-            makedirIfNotExists( path )
-
-            return config()
-
-        } ).then( () => {
-            log( `Going to make the fifo` )
-            return execa( 'mkfifo', [ path + 'ctl' ] )
-                .catch( a => {
-                    log( "Fifo was most likely already there..." )
-                } )
-        } ).then( () => {
+        return config()
+    }).then( () => {
             return youSure( {
                     message: "Would you like to run the advanced installer to get Notifications and keyboard shortcuts?"
                 },
                 advancedInstall
             )
         } )
-    }
+}
+    
 if ( !module.parent ) {
     run()
 }
